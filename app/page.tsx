6c273b737033e5302ce7302192942ea827a91c39
Moburu <stevenschiffner3@gmail.com>
2025-12-02 16:15:21 -0500

    style: slight formatting changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-start justify-start font-sans bg-zinc-800">
+    <div className="flex flex-col min-h-screen items-center justify-center font-sans bg-zinc-800">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-start py-32 px-16 bg-zinc-800 sm:items-start">
         <h1 className="text-6xl">NYCMelee</h1>
         <h2 className="text-3xl">
