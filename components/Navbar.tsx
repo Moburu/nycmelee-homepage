cd05115d24cafff81038762ed1b6cc25445193e7
Hannah Connolly <connollyhannah93@gmail.com>
2025-12-01 22:08:50 -0500

    prettier install

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,56 +1,77 @@
-import * as React from 'react';
+import * as React from "react";
 
 // export interface IAppProps {
 // }
 
-export default function Navbar () {
+export default function Navbar() {
   return (
     <header className="bg-zinc-800">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 items-center justify-between">
-                <div className="hidden md:block">
-                    <nav aria-label="Global">
-                    <ul className="flex items-center gap-6 text-sm">
-                        <li>
-                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="#">
-                            About
-                        </a>
-                        </li>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="hidden md:block">
+            <nav aria-label="Global">
+              <ul className="flex items-center gap-6 text-sm">
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    About
+                  </a>
+                </li>
 
-                        <li>
-                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="#">
-                            Events
-                        </a>
-                        </li>
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    Events
+                  </a>
+                </li>
 
-                        <li>
-                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="#">
-                            Replays
-                        </a>
-                        </li>
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    Replays
+                  </a>
+                </li>
 
-                        <li>
-                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="https://www.youtube.com/c/NYCMelee" target="_blank">
-                            YouTube
-                        </a>
-                        </li>
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="https://www.youtube.com/c/NYCMelee"
+                    target="_blank"
+                  >
+                    YouTube
+                  </a>
+                </li>
 
-                        <li>
-                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="https://www.twitch.tv/nycmelee" target="_blank">
-                            Twitch
-                        </a>
-                        </li>
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="https://www.twitch.tv/nycmelee"
+                    target="_blank"
+                  >
+                    Twitch
+                  </a>
+                </li>
 
-                        <li>
-                        <a className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75" href="https://discord.gg/2aRQ5UUK" target="_blank">
-                            Discord
-                        </a>
-                        </li>
-                    </ul>
-                    </nav>
-                </div>
-            </div>
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="https://discord.gg/2aRQ5UUK"
+                    target="_blank"
+                  >
+                    Discord
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
+      </div>
     </header>
   );
 }
