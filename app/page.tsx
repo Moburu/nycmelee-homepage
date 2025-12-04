30e9d85ca005174c8e972e85646b8bcd02459c61
Moburu <stevenschiffner3@gmail.com>
2025-12-04 17:53:27 -0500

    Added basic skeleton for page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center font-sans bg-zinc-800">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-start py-32 px-16 bg-zinc-800 sm:items-start">
+    <main className="flex min-h-screen flex-col items-stretch justify-start bg-zinc-800 sm:items-start">
+      <div className="flex flex-col gap-4 text-white justify-center items-center bg-blue-500 w-full h-[400px]">
         <h1 className="text-6xl">NYCMelee</h1>
         <h2 className="text-3xl">
           Hosts of Stock Exchange, The Function, and more!
         </h2>
-      </main>
-    </div>
+      </div>
+      <div className="grid grid-cols-3 grid-rows-1 gap-4 text-white items-center justify-center bg-red-500 w-full h-[400px]">
+        <h1 className="text-6xl text-center">NYCMelee</h1>
+        <h1 className="text-6xl text-center">NYCMelee</h1>
+        <h1 className="text-6xl text-center">NYCMelee</h1>
+      </div>
+      <div className="flex flex-row gap-4 text-white justify-around items-center bg-green-500 w-full h-[400px]">
+        <h1 className="text-6xl">Article</h1>
+        <h1 className="text-6xl">Article</h1>
+        <h1 className="text-6xl">Article</h1>
+      </div>
+      <div className="flex flex-row gap-4 text-white justify-around items-center bg-violet-500 w-full h-[400px]">
+        <p className="text-3xl">Discord</p>
+        <p className="text-3xl">Twitter</p>
+        <p className="text-3xl">YouTube</p>
+        <p className="text-3xl">Email</p>
+      </div>
+    </main>
   );
 }
