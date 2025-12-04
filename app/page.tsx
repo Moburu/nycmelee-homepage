fc7921d2f4161b9446771166acff7f9e77033441
Moburu <stevenschiffner3@gmail.com>
2025-12-04 18:39:54 -0500

    Added banner component with background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import Banner from "@/components/Banner";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-stretch justify-start bg-zinc-800 sm:items-start">
-      <div className="flex flex-col gap-4 text-white justify-center items-center bg-blue-500 w-full h-[400px]">
+      <Banner />
+      {/* <div className="flex flex-col gap-4 text-white justify-center items-center bg-blue-500 w-full h-[400px]">
         <h1 className="text-6xl">NYCMelee</h1>
         <h2 className="text-3xl">
           Hosts of Stock Exchange, The Function, and more!
         </h2>
-      </div>
+      </div> */}
       <div className="grid grid-cols-3 grid-rows-1 gap-4 text-white items-center justify-center bg-red-500 w-full h-[400px]">
         <h1 className="text-6xl text-center">NYCMelee</h1>
         <h1 className="text-6xl text-center">NYCMelee</h1>
