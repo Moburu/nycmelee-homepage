4365834cf283ad4b15658dcfee8893fc9071fab4
Hannah Connolly <connollyhannah93@gmail.com>
2025-12-09 04:05:22 -0500

    i made the banner sticky... so dripped

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,27 @@
 import Banner from "@/components/Banner";
+import banner from "../public/banner.jpeg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-start justify-start bg-zinc-800">
-      <Banner />
-      <div className="grid grid-cols-3 grid-rows-1 gap-4 text-white items-center justify-center bg-red-500 w-full h-[400px]">
-        <h1 className="text-6xl text-center">NYCMelee</h1>
-        <h1 className="text-6xl text-center">NYCMelee</h1>
-        <h1 className="text-6xl text-center">NYCMelee</h1>
-      </div>
+    <main>
+      <div
+        className="bg-cover bg-center bg-black/60 bg-blend-multiply bg-fixed"
+        style={{ backgroundImage: `url(${banner.src})` }}
+      >
+        <div className="flex min-h-screen w-full flex-col items-start justify-start">
+          <Banner />
+          <div className="grid grid-cols-3 grid-rows-1 gap-4 text-white items-center justify-center outline-solid w-full h-[400px]">
+            <h1 className="text-6xl text-center">NYCMelee</h1>
+            <h1 className="text-6xl text-center">NYCMelee</h1>
+            <h1 className="text-6xl text-center">NYCMelee</h1>
+          </div>
 
-      <div className="flex flex-row gap-4 text-white justify-around items-center bg-green-500 w-full h-[400px]">
-        <h1 className="text-6xl">Article</h1>
-        <h1 className="text-6xl">Article</h1>
-        <h1 className="text-6xl">Article</h1>
+          <div className="flex flex-row gap-4 text-white justify-around items-center w-full h-[400px]">
+            <h1 className="text-6xl">Article</h1>
+            <h1 className="text-6xl">Article</h1>
+            <h1 className="text-6xl">Article</h1>
+          </div>
+        </div>
       </div>
     </main>
   );
