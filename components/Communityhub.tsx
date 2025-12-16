23fd45d162e4095d6ded55daaf121bcc7d1bb355
Hannah Connolly <connollyhannah93@gmail.com>
2025-12-15 23:16:13 -0500

    community hub update

--- components/Communityhub.tsx
+++ components/Communityhub.tsx
@@ -1,8 +1,8 @@
 export default function CommunityHub() {
   return (
     <div>
-      <h1>Join The Discord</h1>
-      <p>Find matches, housing, and venue updates.</p>
+      <h1>NYSE Intro Text</h1>
+      <p>First timer info</p>
     </div>
   );
 }
