1e2fcd7415e64b98bce5bab1f3b9d4c7fd4ed514
Moburu <stevenschiffner3@gmail.com>
2025-12-16 20:48:34 -0500

    styling for tree list

--- app/replays/page.tsx
+++ app/replays/page.tsx
@@ -3,13 +3,14 @@
 import { createClient } from '../../lib/supabase/client';
 import { useEffect, useState } from "react";
 import { Database } from '@/database.types';
-import TreeMenu from 'react-simple-tree-menu';
+import TreeMenu, { defaultChildren, ItemComponent } from 'react-simple-tree-menu';
 import { TreeMenuProps } from 'react-simple-tree-menu';
 
 export default function Replays() {
     type Replays = Database['public']['Tables']['replays']['Row'];
     const supabase = createClient();
     const [treeData, setTreeData] = useState<TreeMenuProps['data']>([]);
+    const [query, setQuery] = useState('');
 
     useEffect(() => {
       const getReplays = async () => {
@@ -32,7 +33,31 @@ export default function Replays() {
 
     return (
         <div>
-            <TreeMenu data={treeData} />
+            <TreeMenu data={treeData}>
+            {({ search, items, resetOpenNodes }) => (
+                <div className="w-1/2 mx-auto my-10">
+                    <input
+                        onChange={e => search && search(e.target.value)}
+                        placeholder="Type and search"
+                        className="w-full p-2 mb-4 border border-gray-300 rounded"
+                    />
+                    <ul>
+                        {items.map(({key, ...props}) => (
+                            <ItemComponent
+                                key={key}
+                                style={{
+                                    fontSize: '16px',
+                                    border: '1px solid #ccc',
+                                    margin: '5px',
+                                    padding: '5px',
+                                }}
+                                {...props}
+                            />
+                        ))}
+                    </ul>
+                </div>
+            )}
+            </TreeMenu>
         </div>
     );
 }
