d761a04d54c8cc05be5194223513cb0bf24a2182
Hannah Connolly <connollyhannah93@gmail.com>
2025-12-29 22:26:38 -0500

    lmao

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,7 +5,7 @@ export default function Banner() {
     <div className="flex flex-col gap-4 text-white justify-center items-center w-full h-[350px] bg-black/60">
       <h1 className="text-6xl text-shadow-lg/50 font-semibold">NYCMelee</h1>
       <h2 className="text-3xl text-shadow-lg/50">
-        Hosts of Stock Exchange, The Function, and more!
+        Hosts of Stock Exchange, The Function, and more! pingas
       </h2>
     </div>
   );
