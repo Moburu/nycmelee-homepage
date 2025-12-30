ece722d24f4554ecbd9b69e13eb1a0d4e69c2916
Hannah Connolly <connollyhannah93@gmail.com>
2025-12-29 22:21:09 -0500

    event info init

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,6 @@ export default function Home() {
           <StartGGHomePage />
           <div className="flex flex-row gap-4 text-white justify-around items-center w-full h-[400px]">
             <h1 className="text-6xl">NYSE Logo/ hyperlink to sgg</h1>
-            <h1 className="text-6xl">
-              NYSE Signup link / weekly metrics / first time FAQ
-            </h1>
             <StartGGEventInfo />
             <h1 className="text-6xl">Twitch / YT preview?</h1>
           </div>
